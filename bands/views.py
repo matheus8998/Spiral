e5d4194637db7a58da74741a782d56f018a7ad2d
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2021-02-27 01:00:33 -0300

    Criado página de criação de bandas

--- bands/views.py
+++ bands/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Band
 
 # Create your views here.
 def create(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        description = request.POST['description']
+        style = request.POST['style']
+        location = request.POST['location']
+
+        ins = Band(name=name, description=description,
+                style=style, location=location)
+        ins.save()
+        return redirect('dashboard')
+
+
     return render(request, 'band/create.html')
