6a5c76aa2a74ba518cad11c2d7d27742ea10e5e2
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2021-02-27 00:29:16 -0300

    Criado model para bandas chamado Bands

--- bands/models.py
+++ bands/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    style = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
